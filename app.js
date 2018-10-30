9dde7fccc35c14a0064684949ed648e4c50bb772
lhfsidney <lhfsidney@gmail.com>
2018-10-29 20:57:29 -0400

    Add files

---
+++ app.js
@@ -0,0 +1,116 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, activePlayer, ronudScore, gamePlaying, prevDice, winningScore;
+
+init();
+
+
+    
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    if(!gamePlaying){
+        return;
+    }
+    //get a random number
+    var dice = Math.floor(Math.random() *6) + 1;
+    var diceUp = Math.floor(Math.random() *6) + 1;
+
+    //Display the number
+    var domDice = document.querySelector('.dice');
+    var domDiceUp = document.querySelector('.diceUp');
+    
+    domDice.style.display = 'block';
+    domDice.src = 'dice-' + dice + '.png';
+    domDiceUp.style.display = 'block';
+    domDiceUp.src = 'dice-' + diceUp + '.png';
+
+    //Update round
+    if((dice !== 1 && diceUp !== 1)){
+        ronudScore += dice + diceUp; 
+        document.getElementById('current-'+activePlayer).textContent = ronudScore;
+        prevDice = dice;
+    } else {
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    
+    if(!gamePlaying){
+        return;
+    }
+    scores[activePlayer] += ronudScore;
+    document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
+    winningScore = document.getElementById("winningScore").value;
+    
+    if(!winningScore || winningScore < 0 || isNaN(winningScore)){
+        winningScore = 50;
+        document.getElementById("winningScore").value = 50;
+    }
+    
+    if(scores[activePlayer] >= winningScore){
+        document.querySelector('#name-'+activePlayer).textContent = 'Winner!!!'
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+        gamePlaying = false;
+    } else {
+        nextPlayer();
+    }
+})
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    ronudScore = 0;
+            
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    
+    //document.querySelector('.dice').style.display = 'none';
+}
+
+function init(){
+    
+    scores = [0,0];
+    activePlayer = 0;
+    ronudScore = 0;
+    gamePlaying = true;
+    prevDice = 0;
+    
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.diceUp').style.display = 'none';
+    
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    
+    winningScore = document.getElementById('winningScore').value;
+    
+    console.log('Score: '+winningScore);
+    
+    
+}
